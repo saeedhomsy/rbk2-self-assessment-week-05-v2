4b34f62ae19326e89665179aaf76f37a85647811
saeedhomsy <homsysaeed@gmail.com>
2017-02-04 09:16:08 +0200

    second

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -1,5 +1,5 @@
 angular.module('app', ['ngRoute'])
-  .config(function($routeProvider) {
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/fizzbuzz', {
         templateUrl: 'partials/fizzbuzz.html',
@@ -13,11 +13,34 @@ angular.module('app', ['ngRoute'])
         redirectTo: '/fizzbuzz'
       });
   })
-// DO NOT MODIFY CODE ABOVE THIS LINE
+  // DO NOT MODIFY CODE ABOVE THIS LINE
 
-/*  HINT: Make sure your controllers, methods, and variables 
-    are named what $routeProvider and the partials are expecting  */
+  /*  HINT: Make sure your controllers, methods, and variables 
+      are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function(){})
-  .controller('fozzbazzCtrl', function(){});
+  .factory('counter', function () {
+    var increment = function (number) {
+      if (number % 3 === 0 && number % 5 === 0) {
+        return "FIZZBUZZ"
+      }
+      if (number % 3 === 0) {
+        return "FIZZ"
+      }
+      if (number % 5 === 0) {
+        return "BUZZ"
+      }
+      return number;
+    }
+    return {
+      increment: increment
+    }
+  })
+  .controller('fizzbuzzCtrl', function ($scope) {
+    $scope.display = 0;
+    $scope.counter = 0;
+    $scope.increment = function () {
+      $scope.counter++;
+      scope.display = Counter.increment($scope.counter);
+    }
+  })
+  .controller('fozzbazzCtrl', function () { });
